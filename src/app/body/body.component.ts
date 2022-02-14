b337aaa5803e76959e97f541358de58bf1d93056
SheinaHamisi <hamisisheina@gmail.com>
2022-02-14 10:49:21 +0300

    added github added user services

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from '../http-client/github.service';
+
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
+
 })
 export class BodyComponent implements OnInit {
 
-  constructor() { }
+  
+
+  constructor(public userService: GithubService) { }
+
+  submitQuery(userName:string){
+    let user = userName;
+   
+  }
 
   ngOnInit(): void {
   }
